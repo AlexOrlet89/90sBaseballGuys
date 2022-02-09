a9620ed7f4bd0756cf929cfcf70726a9bf6e17fc
Alex Orlet <AlexOrlet89@gmail.com>
2022-02-09 12:24:25 -0800

    added position to player stats and page

--- guys/guys.js
+++ guys/guys.js
@@ -14,7 +14,9 @@ const picture = document.getElementById('picture');
 const name = document.getElementById('name');
 const fact = document.getElementById('fact'); 
 const title = document.querySelector('title');
+const position = document.getElementById('position');
 
+position.textContent =`Position Played: ${dude.position}`;
 name.textContent = `${dude.name}`;
 fact.textContent = `${dude.fact}`;
 title.textContent = `hey, it's ${dude.name}`;
